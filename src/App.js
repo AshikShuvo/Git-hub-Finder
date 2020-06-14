70ab6e8ad69a13696dce38d3cc0197898963ce77
AshikShuvo <ashikshuvo1996@gmail.com>
2020-06-14 16:17:52 +0600

    init final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/user/Users";
+import Search from "./components/user/Search";
+import axios from "axios";
+import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
+import User from "./components/user/User";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state = {
+    users: [],
+    user: {},
+    loading: false,
+    alert: null,
+    repos: [],
+  };
+
+  searchUser = async (text) => {
+    this.setState({ loading: true });
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=0fbab8e9eaf8afb42009
+    & client_secret=b41a85735d78c7642f949282712e225d4296aacf`);
+    this.setState({ users: res.data.items, loading: false });
+  };
+  getUser = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=0fbab8e9eaf8afb42009
+    & client_secret=b41a85735d78c7642f949282712e225d4296aacf`);
+    this.setState({ user: res.data, loading: false });
+  };
+
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=0fbab8e9eaf8afb42009
+    & client_secret=b41a85735d78c7642f949282712e225d4296aacf`);
+    this.setState({ repos: res.data, loading: false });
+  };
+  clearUser = () => this.setState({ users: [], loading: false });
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => this.setState({ alert: null }), 3000);
+  };
+  render() {
+    return (
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <div className='container'>
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUser={this.searchUser}
+                      clearUser={this.clearUser}
+                      showClear={this.state.users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users
+                      loading={this.state.loading}
+                      users={this.state.users}
+                    />
+                  </Fragment>
+                )}
+              />
+              <Route exact path='/about' component={About} />
+              <Route
+                exact
+                path='/user/:login'
+                render={(props) => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
+                    user={this.state.user}
+                    repos={this.state.repos}
+                    loading={this.state.loading}
+                  />
+                )}
+              />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>it is form about component</h1>
+      <p>to check about component is working</p>
+    </Fragment>
+  );
+};
+
+export default About;
